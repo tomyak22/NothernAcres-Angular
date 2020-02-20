fb67aee9cecf8c4fb38b9ca825726ea83414062b
Tom Fenyak <tommyfenyak@gmail.com>
2020-02-19 19:28:01 -0500

    added footer

--- northern-acres-app/src/app/app.module.ts
+++ northern-acres-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FeatureCarouselComponent } from './feature-carousel/feature-carousel.component';
 import { HomeTextComponent } from './home-text/home-text.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
 
 
 
@@ -14,7 +15,8 @@ import { HomeTextComponent } from './home-text/home-text.component';
     AppComponent,
     NavBarComponent,
     FeatureCarouselComponent,
-    HomeTextComponent
+    HomeTextComponent,
+    FooterSectionComponent
   ],
   imports: [
     BrowserModule,
